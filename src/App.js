247cca03700c152e82994f730ed0a0decbc06874
DevChirayu <chirayupatil23@gmail.com>
2025-01-28 22:51:54 +0530

    qr logic added

--- src/App.js
+++ src/App.js
@@ -1,61 +1,98 @@
-// Filename - App.js
+import React, { useState } from "react";
+import QRCode from "qrcode.react"; // Import QRCode component
+import { QRCodeCanvas } from 'qrcode.react';
 
-import { useState } from 'react';
-import QRCode from 'react-qr-code';
 
 function App() {
-    const [value, setValue] = useState();
-    const [back, setBack] = useState('#FFFFFF');
-    const [fore, setFore] = useState('#000000');
-    const [size, setSize] = useState(256);
+  // State for UPI parameters
+  const [payeeVpa, setPayeeVpa] = useState("7738482563@amazonpay");
+  const [payeeName, setPayeeName] = useState("Chirayu Patil");
+  const [amount, setAmount] = useState("1000");
+  const [transactionNote, setTransactionNote] = useState("Donation");
 
-    return (
-        <div className="App">
-            <center>
-                <br />
-                <br />
-                <input
-                    type="text"
-                    onChange={(e) => setValue(e.target.value)}
-                    placeholder="Value of Qr-code"
-                />
-                <br />
-                <br />
-                <input
-                    type="text"
-                    onChange={(e) => setBack(e.target.value)}
-                    placeholder="Background color"
-                />
-                <br />
-                <br />
-                <input
-                    type="text"
-                    onChange={(e) => setFore(e.target.value)}
-                    placeholder="Foreground color"
-                />
-                <br />
-                <br />
-                <input
-                    type="number"
-                    onChange={(e) => setSize(parseInt(e.target.value ===
-                        '' ? 0 : e.target.value, 10))}
-                    placeholder="Size of Qr-code"
-                />
-                <br />
-                <br />
-                <br />
-                {value && (
-                    <QRCode
-                        title="GeeksForGeeks"
-                        value={value}
-                        bgColor={back}
-                        fgColor={fore}
-                        size={size === '' ? 0 : size}
-                    />
-                )}
-            </center>
+  // Generate UPI URL
+  const generateUpiUrl = () => {
+    return `upi://pay?pa=${payeeVpa}&pn=${payeeName}&tn=${transactionNote}&am=${amount}&cu=INR`;
+  };
+
+  return (
+    <div className="form-container" >
+      <h2>Generate UPI QR Code</h2>
+
+      {/* Form to enter UPI parameters */}
+      <form>
+        <div>
+          <label>Payee UPI ID</label>
+          <input
+            type="text"
+            value={payeeVpa}
+            onChange={(e) => setPayeeVpa(e.target.value)}
+            placeholder="7738482563@amazonpay"
+            required
+          />
         </div>
-    );
+
+        <div>
+          <label>Payee Name</label>
+          <input
+            type="text"
+            value={payeeName}
+            onChange={(e) => setPayeeName(e.target.value)}
+            placeholder="Demo Payee"
+            required
+          />
+        </div>
+
+        <div>
+          <label>Amount</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="10"
+          />
+        </div>
+
+        <div>
+          <label>Transaction Note</label>
+          <input
+            type="text"
+            value={transactionNote}
+            onChange={(e) => setTransactionNote(e.target.value)}
+            placeholder="Demo Transaction"
+          />
+        </div>
+      </form>
+
+      <div className="qr-code-container" >
+        <h3>{payeeName.toUpperCase()}</h3>
+      <QRCodeCanvas value={generateUpiUrl()} size={256} />
+      <h5>{payeeVpa}</h5>
+      <h2>Scan and pay with any UPI app</h2>
+      <div className="image-container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/6/65/BHIM_SVG_Logo.svg/320px-BHIM_SVG_Logo.svg.png"
+          alt="BHIM Logo"
+          className="logo"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6f/UPI_logo.svg"
+          alt="UPI Logo"
+          className="logo"
+        />
+      </div>
+
+
+      </div>
+
+      {/* <div>
+        <h4>Generated UPI URL:</h4>
+        <p>{generateUpiUrl()}</p>
+        </div> */}
+
+{/* <image src="https://upload.wikimedia.org/wikipedia/commons/6/6f/UPI_logo.svg"></image> */}
+    </div>
+  );
 }
 
 export default App;
