a0db8744a135774655e8de593f21826de6c1c49d
DevChirayu <chirayupatil23@gmail.com>
2025-01-29 19:55:06 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import QRCode from "qrcode.react"; // Import QRCode component
-import { QRCodeCanvas } from 'qrcode.react';
-
+import { QRCodeCanvas } from "qrcode.react";
 
 function App() {
   // State for UPI parameters
@@ -16,73 +15,75 @@ function App() {
   };
 
   return (
-    <div className="form-container" >
+    <div className="form-container">
       <h2>Generate UPI QR Code</h2>
 
       {/* Form to enter UPI parameters */}
       <form>
-        <div>
+        {/* <div> */}
           <label>Payee UPI ID</label>
           <input
             type="text"
             value={payeeVpa}
-            onChange={(e) => setPayeeVpa(e.target.value)}
+            onChange={e => setPayeeVpa(e.target.value)}
             placeholder="7738482563@amazonpay"
             required
           />
-        </div>
+        {/* </div> */}
 
-        <div>
+        {/* <div> */}
           <label>Payee Name</label>
           <input
             type="text"
             value={payeeName}
-            onChange={(e) => setPayeeName(e.target.value)}
+            onChange={e => setPayeeName(e.target.value)}
             placeholder="Demo Payee"
             required
           />
-        </div>
+        {/* </div> */}
 
-        <div>
+        {/* <div> */}
           <label>Amount</label>
           <input
             type="number"
             value={amount}
-            onChange={(e) => setAmount(e.target.value)}
+            onChange={e => setAmount(e.target.value)}
             placeholder="10"
           />
-        </div>
+        {/* </div> */}
 
-        <div>
+        {/* <div> */}
           <label>Transaction Note</label>
           <input
             type="text"
             value={transactionNote}
-            onChange={(e) => setTransactionNote(e.target.value)}
+            onChange={e => setTransactionNote(e.target.value)}
             placeholder="Demo Transaction"
           />
-        </div>
+        {/* </div> */}
       </form>
 
-      <div className="qr-code-container" >
-        <h3>{payeeName.toUpperCase()}</h3>
-      <QRCodeCanvas value={generateUpiUrl()} size={256} />
-      <h5>{payeeVpa}</h5>
-      <h2>Scan and pay with any UPI app</h2>
-      <div className="image-container">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/en/thumb/6/65/BHIM_SVG_Logo.svg/320px-BHIM_SVG_Logo.svg.png"
-          alt="BHIM Logo"
-          className="logo"
-        />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/6f/UPI_logo.svg"
-          alt="UPI Logo"
-          className="logo"
-        />
-      </div>
-
-
+      <div className="qr-code-container">
+        <h3>
+          {payeeName.toUpperCase()}
+        </h3>
+        <QRCodeCanvas value={generateUpiUrl()} size={256} />
+        <h5>
+          {payeeVpa}
+        </h5>
+        <h2>Scan and pay with any UPI app</h2>
+        <div className="image-container">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/6/65/BHIM_SVG_Logo.svg/320px-BHIM_SVG_Logo.svg.png"
+            alt="BHIM Logo"
+            className="logo"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/6f/UPI_logo.svg"
+            alt="UPI Logo"
+            className="logo"
+          />
+        </div>
       </div>
 
       {/* <div>
@@ -90,7 +91,7 @@ function App() {
         <p>{generateUpiUrl()}</p>
         </div> */}
 
-{/* <image src="https://upload.wikimedia.org/wikipedia/commons/6/6f/UPI_logo.svg"></image> */}
+      {/* <image src="https://upload.wikimedia.org/wikipedia/commons/6/6f/UPI_logo.svg"></image> */}
     </div>
   );
 }
